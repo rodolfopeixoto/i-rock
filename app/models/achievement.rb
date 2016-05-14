4b7185ba5fbcd04e86694d979ea8138032785742
Rodolfo Peixoto - DELLI7INSPIRION <contato@rodolfopeixot.com.br>
2016-05-14 20:49:23 -0300

    Test validatios Model Test

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -3,10 +3,19 @@ class Achievement < ActiveRecord::Base
     belongs_to :user
  
     validates :title, presence: true 
+    # validates :title, uniqueness: true
+    validate :unique_title_for_one_user
     enum privacy: [:public_access, :private_access, :friend_access]
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
+  end
+
+  def unique_title_for_one_user
+    existing_achievement = Achievement.find_by(title: title)
+    if existing_achievement && existing_achievement.user == user
+      errors.add(:title, "you can't have two achievements with the same title")
+    end
   end
  
 end
