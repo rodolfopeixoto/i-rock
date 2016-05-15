daa5e10a73885f04dbd8c023ab248ffa814a5b6f
Rodolfo Peixoto - DELLI7INSPIRION <contato@rodolfopeixot.com.br>
2016-05-14 22:30:23 -0300

    Delete all comments

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -8,18 +8,11 @@ class Achievement < ActiveRecord::Base
         scope: :user_id,
         message: "you can't have two achievements with the same title"
     }
-    # validate :unique_title_for_one_user
-    enum privacy: [:public_access, :private_access, :friend_access]
+     enum privacy: [:public_access, :private_access, :friend_access]
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(description)
   end
-
-  # def unique_title_for_one_user
-  #   existing_achievement = Achievement.find_by(title: title)
-  #   if existing_achievement && existing_achievement.user == user
-  #     errors.add(:title, "you can't have two achievements with the same title")
-  #   end
-  # end
+ 
  
 end
